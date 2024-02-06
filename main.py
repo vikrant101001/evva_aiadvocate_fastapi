3d61bf6c6af98dd0b2efd60f967154b5ece6b9d6
vikrant101001 <71218338+vikrant101001@users.noreply.github.com>
2024-02-06 10:24:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import shutil
 from datetime import datetime
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware as StarletteCORSMiddleware
+from docx import Document
 
 # Add more permissive CORS settings during development
 origins = ["*"]
@@ -28,12 +29,16 @@ def upload_to_s3(file_path, s3_bucket, s3_key):
     command = f"aws s3 cp {file_path} s3://{s3_bucket}/{s3_key}"
     with subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE) as process:
         process.communicate()
-
-@app.post("/upload")
+        
+@app.get("/")
+def index():
+  return "S3 API Online"
+    
+@app.post("/uploads3")
 async def upload_file_to_s3(req: Request):
     try:
         # Receive 'id' from the JSON request body
-        id = (await req.json())["id"]  # Use await to get the result of the coroutine
+        id = (await req.json())["id"]
 
         if not id:
             raise HTTPException(status_code=400, detail="Missing 'id' in the request body")
@@ -43,9 +48,10 @@ async def upload_file_to_s3(req: Request):
         file_name = os.path.abspath(f"VATest-Evva_Health_{current_date}_{id}.docx")
         s3_key = f"01-028/VATest-Evva_Health_{current_date}_{id}.docx"
 
-        # Copy the existing Word document to the new file name
-        original_file_name = "VATest-Evva_Health-2Feb2024.docx"
-        shutil.copy(original_file_name, file_name)
+        # Create a new Word document with content "evva health"
+        document = Document()
+        document.add_paragraph("evva health")
+        document.save(file_name)
 
         # Upload the file to S3
         upload_to_s3(file_name, "naii-01-dir", s3_key)
@@ -55,6 +61,7 @@ async def upload_file_to_s3(req: Request):
         return JSONResponse(content={"message": str(http_exc.detail)}, status_code=http_exc.status_code)
     except Exception:
         return JSONResponse(content={"message": "Internal Server Error"}, status_code=500)
+
 
 if __name__ == "__main__":
     import uvicorn
