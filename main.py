0b6ab75249f5ccfe91d8cec89958639f199de262
vikrant101001 <71218338+vikrant101001@users.noreply.github.com>
2024-02-12 22:01:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -251,5 +251,5 @@ def upload_file_to_s3():
         print("error:", e)
         return jsonify({"message": "Internal Server Error","error":str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
